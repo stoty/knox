4a054647c79dbf4509d26f5242f09cdd4d0b08f0
Kevin Risden <krisden@cloudera.com>
2020-03-10 16:25:00 -0400

    CDPD-8936 - KNOX-2285 - Change gateway.server.header.enabled default to false
    Signed-off-by: Kevin Risden <krisden@apache.org>
    Signed-off-by: Kevin Risden <krisden@cloudera.com>
    Change-Id: I81063173895adff7e5a7982c069e65062651f5f6

--- gateway-server/src/main/java/org/apache/knox/gateway/config/impl/GatewayConfigImpl.java
+++ gateway-server/src/main/java/org/apache/knox/gateway/config/impl/GatewayConfigImpl.java
@@ -956,7 +956,7 @@ public class GatewayConfigImpl extends Configuration implements GatewayConfig {
 
   @Override
   public boolean isGatewayServerHeaderEnabled() {
-    return Boolean.parseBoolean(getVar(SERVER_HEADER_ENABLED, "true"));
+    return Boolean.parseBoolean(getVar(SERVER_HEADER_ENABLED, "false"));
   }
 
   @Override
