cef08ccdda29f22499f7ffea0746fefc0450cf1b
Sandor Molnar <smolnar@cloudera.com>
2020-03-27 10:41:27 +0100

    CDPD-9477 - Knox parses shared provider configuration from Hadoop XML type descriptors
    Main changes in this commit:
    - implemented a language that Knox understands when parsing shared provider configuration(s) in Haddop XML type descriptors
    - XML/JSON provider configurations are moved out to their own classes from ProviderConfigurationParser
    - XML/JSON provider configuration classes implement hashCode/equals properly
    - XML/JSON provider classes implement Comparable (based on a predefined provider order) as well as fixing the order of fields and alphabetical order withing parameters when serializing
    - changed the separator in Hadoop XML type descriptors from semicolon (;) to hash (#) to conform new requirements
    
    Change-Id: I998af059b5989be2561d6d92faa581188af6dfb9

--- gateway-server/src/main/java/org/apache/knox/gateway/GatewayServer.java
+++ gateway-server/src/main/java/org/apache/knox/gateway/GatewayServer.java
@@ -727,7 +727,7 @@ public class GatewayServer {
   }
 
   private void handleClouderaManagerDescriptors() {
-    final ClouderaManagerDescriptorParser cmDescriptorParser = new ClouderaManagerDescriptorParser();
+    final ClouderaManagerDescriptorParser cmDescriptorParser = new ClouderaManagerDescriptorParser(config);
     final ClouderaManagerDescriptorMonitor cmDescriptorMonitor = new ClouderaManagerDescriptorMonitor(config, cmDescriptorParser);
     final AdvancedServiceDiscoveryConfigurationMonitor advancedServiceDiscoveryConfigurationMonitor = new AdvancedServiceDiscoveryConfigurationMonitor(config);
     advancedServiceDiscoveryConfigurationMonitor.registerListener(cmDescriptorParser);
