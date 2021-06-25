2b504dfed514e6be6f005862463d0c74ba651301
Sandor Molnar <smolnar@apache.org>
2021-06-25 08:52:41 +0200

    CDPD-27417 KNOX-2623 - Lifespan attributes are optional on token generation UI and default to 1 hour (#460)
    Change-Id: Id59e1f171a730c676c98876cf09d423ff34ccb00

--- gateway-service-knoxtoken/src/test/java/org/apache/knox/gateway/service/knoxtoken/TokenServiceResourceTest.java
+++ gateway-service-knoxtoken/src/test/java/org/apache/knox/gateway/service/knoxtoken/TokenServiceResourceTest.java
@@ -856,6 +856,7 @@ public class TokenServiceResourceTest {
     if (expectedAllowedTssFlag != null) {
       assertEquals(statusMap.get("allowedTssForTokengen"), expectedAllowedTssFlag);
     }
+    assertTrue(Boolean.parseBoolean(statusMap.get("lifespanInputEnabled")));
   }
 
   @Test
